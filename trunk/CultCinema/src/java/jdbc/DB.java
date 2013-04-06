97da3eca7361f0d09732f12898116653f78da43a
yanlzhang3@gmail.com <yanlzhang3@gmail.com@06d5ad60-411d-b67e-bec9-518e9217d82a>
2013-04-06 14:06:44 +0000

    jdbc

---
+++ trunk/CultCinema/src/java/jdbc/DB.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jdbc;
+
+/**
+ *
+ * @author Zhang Yanlin
+ */
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class DB {
+  public static DataSource getDataSource() throws NamingException {
+    Context initCtx = new InitialContext();
+    Context envCtx = (Context)initCtx.lookup("java:comp/env");
+    return (DataSource)envCtx.lookup("jdbc/cinema");
+  }
+}
