2861136bdac4370e584c2898bc2844bbbac9a408
yanlzhang3@gmail.com <yanlzhang3@gmail.com@06d5ad60-411d-b67e-bec9-518e9217d82a>
2013-04-06 12:56:54 +0000

    beans

---
+++ trunk/CultCinema/src/java/cs4280/bean/Manage.java
@@ -0,0 +1,75 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs4280.bean;
+
+/**
+ *
+ * @author Zhang Yanlin
+ */
+public class Manage {
+    private int managerID;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+    private String name;
+    private String address;
+    private String tel;
+    private String gender;
+    private String mail;
+    public int getManagerID(){
+        return this.managerID;
+    }
+    public void setManagerID(int managerID){
+        this.managerID=managerID;
+    }
+    
+    
+}
